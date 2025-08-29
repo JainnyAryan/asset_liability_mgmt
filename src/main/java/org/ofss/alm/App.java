cbff494aee4c50c19d6f32f3c806fef05f28defa
JainnyAryan <aryan.one0793@gmail.com>
2025-08-29 16:25:10 +0530

    Init ALM project

---
+++ src/main/java/org/ofss/alm/App.java
@@ -0,0 +1,13 @@
+package org.ofss.alm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/ofss/alm/models/Customer.java
@@ -0,0 +1,11 @@
+package org.ofss.alm.models;
+
+public class Customer {
+    private Long customerId;
+    private String name;
+    private String type; // Individual, Corporate, etc.
+    private String contactInfo;
+
+    // Getters & Setters
+    // ...
+}
